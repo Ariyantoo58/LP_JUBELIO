0e5d77c5641a9cd73d749eb0a69d5c8bed046304
ShofiyahN <sofiyahnbs@gmail.com>
2023-05-23 17:43:14 +0700

    update product

--- src/pages/home/sectionOne.jsx
+++ src/pages/home/sectionOne.jsx
@@ -1,187 +1,153 @@
 import {
-    Box,
-    Button,
-    Container,
-    Flex,
-    Heading,
-    Image,
-    Stack,
-    Text,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-  import { ReactElement } from 'react';
-  import {
-    FcBusiness,
-    FcCalculator,
-    FcFactory,
-    FcPicture,
-    FcPuzzle,
-  } from 'react-icons/fc';
-  import image1 from '../../assets/logo Deoapp/Logo Deoapp (Abu-Abu) Finance.png'
-  import image2 from '../../assets/logo Deoapp/Logo Deoapp (Abu-Abu) HR management.png'
-  import image3 from '../../assets/logo Deoapp/Logo Deoapp (Abu-Abu) PM.png'
-  import image4 from '../../assets/logo Deoapp/Logo Deoapp (Abu-Abu) POS.png'
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Icon,
+  Stack,
+  Text,
+  Image,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import {
+  FcBusiness,
+  FcCalculator,
+  FcComments,
+  FcFactory,
+  FcMoneyTransfer,
+  FcPicture,
+  FcPuzzle,
+  FcShipped,
+} from 'react-icons/fc';
 
-  const CardProps = {
-    heading: '',
-    description: '',
-    icon: null,
-    href: ''
-  };
+import image1 from '../../assets/logo Deoapp/Logo Deoapp 1 CRM.png'
+import image2 from '../../assets/logo Deoapp/Logo Deoapp 1 Finance.png'
+import image3 from '../../assets/logo Deoapp/Logo Deoapp 1 HR Management.png'
+import image4 from '../../assets/logo Deoapp/Logo Deoapp 1 Marketer.png'
+import image5 from '../../assets/logo Deoapp/Logo Deoapp 1 marketing.png'
+import image6 from '../../assets/logo Deoapp/Logo Deoapp 1 POS.png'
+import image7 from '../../assets/logo Deoapp/Logo Deoapp 1 Project Management.png'
+import image8 from '../../assets/logo Deoapp/Logo Deoapp 1 social media.png'
 
-  function SectionOne () {
-    return (
-        <Box p={5}>
-            <Stack spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
-                <Heading fontSize={'3xl'}>OUR PRODUCT</Heading>
-            </Stack>
-            <Container maxW={'5xl'} mt={1} padding={'2%'}>
-            <Flex flexWrap="wrap" gridGap={12} justify="center">
-              <Flex 
-              h={80}
-              maxW={{ base: 'full', md: '800px', lg: '1100px' }}
-              w={'full'}
-              borderWidth="1px"
-              borderRadius="lg"
-              overflow="hidden"
-              boxShadow= {'rgba(0, 0, 0, 0.24) 0px 3px 8px;'}
-              display='flex'
-              >
-                <Flex 
-                w='50%'
-                alignItems='center'
-                justifyContent='center'
-                display='flex'
-                flexDirection='column'>
-                    <Box w='70%' m='auto'>
-                        <Text
-                        fontSize="15pt" fontWeight="bold" textAlign='left'>
-                        Finance
-                        </Text>
-                        <Text color='grey' textAlign='left' mt={5}>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates recusandae natus nihil molestiae. Iure expedita harum, accusantium fuga, eos temporibus eum optio autem dicta nisi impedit ad, est nesciunt magni!
-                        </Text>
+const CardProps = {
+  heading: '',
+  description: '',
+  icon: null,
+  href: ''
+};
 
-                    </Box>
-                </Flex>
-                <Flex 
-                w='50%'borderLeft={'1px solid #e3f4f4'} 
-                bg="#E3F4F4"
-                alignItems='center'
-                justifyContent='center'>
-                    <Image 
-                    w='70%'
-                    src={image1}/>
-                </Flex>
-              </Flex>
+const Card = ({ heading, description, image, href }) => {
+  return (
+    <Box
+      maxW={{ base: 'full', md: '275px', lg: '250px' }}
+      w={'full'}
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      p={6}>
+      <Stack align={'start'} spacing={2}>
+      <Box>
+        <Image src={image}/>
+      </Box>
+        <Box mt={2}>
+          <Heading size="md" h={'10vh'}>{heading}</Heading>
+          <Text fontSize={'sm'} h={'15vh'}>
+            {description}
+          </Text>
+        </Box>
+        <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
+          Cek Fitur
+        </Button>
+      </Stack>
+    </Box>
+  )
+}
 
-              <Flex 
-              h={80}
-              maxW={{ base: 'full', md: '800px', lg: '1100px' }}
-              w={'full'}
-              borderWidth="1px"
-              borderRadius="lg"
-              overflow="hidden"
-              boxShadow= {'rgba(0, 0, 0, 0.24) 0px 3px 8px;'}
-              display='flex'
-              >
-                <Flex w='50%' bg="#E3F4F4"
-                alignItems='center'
-                justifyContent='center'>
-                    <Image 
-                    w='70%'
-                    src={image2}/>
-                </Flex>
-                <Flex w='50%'borderLeft={'1px solid #E3F4F4'} >
-                <Box w='70%' m='auto'>
-                        <Text
-                        fontSize="15pt" fontWeight="bold" textAlign='left'>
-                        HR Managemnt
-                        </Text>
-                        <Text color='grey' textAlign='left' mt={5}>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea blanditiis veritatis, odit quaerat tenetur totam illum? Sapiente, nesciunt minima. Ipsam magni pariatur recusandae aut voluptatum! Cupiditate sed laborum ad accusamus!
-                        </Text>
-
-                    </Box>
-                </Flex>
-                
-              </Flex>
-
-              <Flex 
-              h={80}
-              maxW={{ base: 'full', md: '800px', lg: '1100px' }}
-              w={'full'}
-              borderWidth="1px"
-              borderRadius="lg"
-              overflow="hidden"
-              boxShadow= {'rgba(0, 0, 0, 0.24) 0px 3px 8px;'}
-              display='flex'
-              >
-                <Flex 
-                w='50%'
-                alignItems='center'
-                justifyContent='center'
-                display='flex'
-                flexDirection='column'>
-                    <Box w='70%' m='auto'>
-                        <Text
-                        fontSize="15pt" fontWeight="bold" textAlign='left'>
-                        Project Management
-                        </Text>
-                        <Text color='grey' textAlign='left' mt={5}>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates recusandae natus nihil molestiae. Iure expedita harum, accusantium fuga, eos temporibus eum optio autem dicta nisi impedit ad, est nesciunt magni!
-                        </Text>
-
-                    </Box>
-                </Flex>
-                <Flex 
-                w='50%'borderLeft={'1px solid #e3f4f4'} 
-                bg="#E3F4F4"
-                alignItems='center'
-                justifyContent='center'>
-                    <Image 
-                    w='70%'
-                    src={image3}/>
-                </Flex>
-              </Flex>
-
-              <Flex 
-              h={80}
-              maxW={{ base: 'full', md: '800px', lg: '1100px' }}
-              w={'full'}
-              borderWidth="1px"
-              borderRadius="lg"
-              overflow="hidden"
-              boxShadow= {'rgba(0, 0, 0, 0.24) 0px 3px 8px;'}
-              display='flex'
-              >
-                <Flex w='50%' bg="#E3F4F4"
-                alignItems='center'
-                justifyContent='center'>
-                    <Image 
-                    w='70%'
-                    src={image4}/>
-                </Flex>
-                <Flex w='50%'borderLeft={'1px solid #E3F4F4'} >
-                <Box w='70%' m='auto'>
-                        <Text
-                        fontSize="15pt" fontWeight="bold" textAlign='left'>
-                        POS
-                        </Text>
-                        <Text color='grey' textAlign='left' mt={5}>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea blanditiis veritatis, odit quaerat tenetur totam illum? Sapiente, nesciunt minima. Ipsam magni pariatur recusandae aut voluptatum! Cupiditate sed laborum ad accusamus!
-                        </Text>
-
-                    </Box>
-                </Flex>
-                
-              </Flex>
-
-              </Flex>
-              </Container>
+function SectionOne() {
+  return (
+    <>
+      <Box w={{ base: '100%', lg: '60%' }} m={'auto'}>
+        <Box p={4} w={'100%'}>
+          <Stack spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
+            <Heading fontSize={'3xl'}>Our Product</Heading>
+          </Stack>
 
         </Box>
-      )
+      </Box>
 
-  }
-  export default SectionOne
+      <Box p={5}>
+        <Container maxW={'7xl'} mt={2}>
+          <Flex flexWrap="wrap" gap={10} justify="center">
+            <Card
+              heading={'CRM'}
+              description={
+                'Kontrol semua operasional bisnis dalam satu platform terintegrasi.'
+              }
+              image={image1}
+            />
+            <Card
+              heading={'Finance'}
+
+              description={
+                'Visibilitas dan kontrol penuh kelola gudang langsung lewat aplikasi.'
+              }
+              image={image2}
+            />
+            <Card
+              heading={'HR Management'}
+
+              description={
+                'Aplikasi kasir gratis jualan online dan offline agar bisnis berjalan lebih optimal.'
+              }
+              image={image3}
+            />
+            <Card
+              heading={'Marketer'}
+
+              description={
+                'Buat toko online dengan desain premium tanpa keluarkan biaya apa pun.'
+              }
+              image={image4}
+            />
+            <Card
+              heading={'Marketing'}
+
+              description={
+                'Urus pemenuhan orderan lebih nyaman dan aman dengan partner terbaik.'
+              }
+              image={image5}
+            />
+            <Card
+              heading={'POS'}
+
+              description={
+                'Sambungkan pesan dan berinteraksi dengan pelanggan di berbagai marketplace lewat satu platform.'
+              }
+              image={image6}
+            />
+            <Card
+              heading={'Product Management'}
+
+              description={
+                'Urus pemenuhan orderan lebih nyaman dan aman dengan partner terbaik.'
+              }
+              image={image7}
+            />
+            <Card
+              heading={'Social Media'}
+
+              description={
+                'Sambungkan pesan dan berinteraksi dengan pelanggan di berbagai marketplace lewat satu platform.'
+              }
+              image={image8}
+            />
+
+          </Flex>
+        </Container>
+
+      </Box>
+    </>
+  )
+}
+export default SectionOne
