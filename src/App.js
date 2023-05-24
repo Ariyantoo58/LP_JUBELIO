a788daf36252e16b89a26890689b6978d2aad861
ShofiyahN <sofiyahnbs@gmail.com>
2023-05-24 16:03:39 +0700

    yang inii

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import RouteApp from './Routes/RoutesApp';
 import { Box, ChakraProvider, theme } from '@chakra-ui/react';
 import Footer from './components/footer';
 import Navbar from './components/navbar/index2';
-import CRM from './pages/produk/CRM';
-import Finance from './pages/produk/Finance';
-import HR from './pages/produk/HR Management';
-import ProjectManagement from './pages/produk/Project Management';
-import Marketing from './pages/produk/Marketing';
 
 
 function App() {
@@ -16,7 +11,6 @@ function App() {
       <Navbar />
       <Box mt={{ base: '70px', lg: '95px' }}>
         <RouteApp />
-        {/* <Marketing/> */}
       </Box>
 
       <Footer />
